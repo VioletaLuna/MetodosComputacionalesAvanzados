d3739d8c7f817f70823858cdd5888df4f12649c5
VioletaLuna <32911004+VioletaLuna@users.noreply.github.com>
2018-11-23 06:56:07 -0500

    Paralelisado

--- ejercicios/walk.c
+++ ejercicios/walk.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-
+#include <opm.h>PA
 
 int walk(double barrier, int seed);
 double *reserva(int n_points);
 void print(double *x, int n_points);
 
-void main (){ 
+void main ()
+{ 
   double *A;
   int i;
   int n_walkers = 500000;
 
   A = reserva(n_walkers);     
-  for (i=0; i<n_walkers; i++){
-     A[i] = walk(10.0, i);
+  #pragma omp parallel
+  {
+    for (i=0; i<n_walkers; i++){
+       A[i] = walk(10.0, i);
+    }
+    print(A,n_walkers);
   }
-  print(A,n_walkers);
 }
 
-int walk(double barrier, int seed){
-
+int walk(double barrier, int seed)
+{
   double pos;
   int n_steps;
   srand48(seed);
@@ -35,22 +38,25 @@ int walk(double barrier, int seed){
   return n_steps;
 }
 
-
-void print(double *x, int n_points){
+void print(double *x, int n_points)
+{
   int i;
-  for(i = 0 ; i < n_points ; i++){
+  for(i = 0 ; i < n_points ; i++)
+  {
     fprintf(stdout, "%f\n", x[i]);
   }
 }
-
-double *reserva(int n_points){
-  double *x;
-  int i;
-  if(!(x=malloc(sizeof(double) * n_points))){
-    printf("malloc problem\n");
-    exit(1);
-}
-  for(i=0;i<n_points;i++){
+double *reserva(int n_points)
+{
+    double *x;
+    int i;
+    if(!(x=malloc(sizeof(double) * n_points)))
+    {
+      printf("malloc problem\n");
+      exit(1);
+    }
+  for(i=0;i<n_points;i++)
+  {
     x[i] = 0.0;
   }
   return x;
